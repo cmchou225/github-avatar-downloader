a588ddbf93882c6bd4a607690973039c28bb7f3a
Charles <cming.chou@gmail.com>
2017-04-11 04:47:31 +0000

    cleaned up code for final submission

--- download_avatars.js
+++ download_avatars.js
@@ -1,16 +1,15 @@
-const request = require('request');
-const fs = require('fs');
-
-const repoOwner = process.argv[2];
-const repoName = process.argv[3];
+const request = require('request'),
+      fs = require('fs'),
+      git = require('./gitToken.js'),
+      repoOwner = process.argv[2],
+      repoName = process.argv[3];
 
 console.log('Welcome to the GibHub Avatar Downloader!');
 
+const GITHUB_USER = git.username,
+      GITHUB_TOKEN = git.token;
 
-
-const GITHUB_USER = 'cmchou225',
-      GITHUB_TOKEN = '94de8947e1520cf2ab222b5781915d39dd017c03';
-
+//Main function to download avatar pictures  with repo owner and name input
 function getRepoContributors(repoOwner, repoName, cb) {
   if(!repoOwner || !repoName)
     return console.log("Please enter both the repository owner and repository.");
@@ -20,40 +19,32 @@ function getRepoContributors(repoOwner, repoName, cb) {
       'User-Agent':  "GitHub Avatar Downloader - Student Project"
     }
   }
-  let content;
   request(options, function(err, response, body) {
-    content = JSON.parse(body);
-    console.log(content);
+    const content = JSON.parse(body);
     cb(content);
   });
 
-
 }
 
+//callback function to loop through contributor objects extracting the avatar URL and downloads pictures into avatar folder
 function callback (array){
-
   array.forEach(function (obj){
     let path = `avatars/${obj['login']}.jpg`;
     downloadImageByURL(obj['avatar_url'], path);
   });
 }
 
+//helper function to download avatar pictures with url and filepath input
 function downloadImageByURL(url, filePath){
   request.get(url)
   .on('error' , function (err) {
-    throw err;
+    return console.log("Error found: ", err);
    })
   .on('response', function (response) {
     console.log('Response Status Code: ', response.statusMessage, 'Response content ', response.headers['content-type']);
    })
   .pipe(fs.createWriteStream(filePath))
 }
-// downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg");
-
-// getRepoContributors("jquery", "jquery", function (err, result){
-//   console.log('Errors', err);
-//   console.log('result:', result);
-// });
 
 getRepoContributors(repoOwner, repoName, callback);
 
