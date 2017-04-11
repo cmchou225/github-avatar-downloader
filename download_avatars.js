60e3f383efcf142d78f4c5f26a6a7427be6e0106
Charles <cming.chou@gmail.com>
2017-04-11 02:18:00 +0000

    print full body of request  successful

--- download_avatars.js
+++ download_avatars.js
@@ -3,17 +3,42 @@ const fs = require('fs');
 
 console.log('Welcome to the GibHub Avatar Downloader!');
 
+// 'https://cmchou225@c2bbe98c0d47f95a8ca29d56e21634093f79f8c4api.github.com/repos/jquery/jquery/contributors'
+
 const GITHUB_USER = "cmchou225",
       GITHUB_TOKEN = "c2bbe98c0d47f95a8ca29d56e21634093f79f8c4";
 
-function getRepoContributors(repoOwner, repoName, cb) {
-  let requestURL = `https://${GITHUB_USER}:${GITHUB_TOKEN}@api.github.com/repos/${repoOwner}/${repoName}/contributors`;
-  console.log(requestURL);
+function getRepoContributors(repoOwner, repoName) {
+
+  const options = {
+    url: `https://${GITHUB_USER}:${GITHUB_TOKEN}@api.github.com/repos/${repoOwner}/${repoName}/contributors`,
+    headers: {
+      'User-Agent':  "GitHub Avatar Downloader - Student Project"
+    }
+  }
+
+  let body = "";
+  request(options)
+  .on('error' , function (err) {
+    console.log(`Error ${err}`)
+    throw err;
+   })
+  .on('response', function (response) {
+    console.log(`${response.statusCode} ${response.statusMessage}`)
+  })
+  .on('data', function (chunk) {
+    body += chunk;
+  })
+  .on('end', function () {
+    console.log(JSON.parse(body));
+  })
 }
 
-getRepoContributors("jquery", "jquery", function (err, result){
-  console.log('Errors', err);
-  console.log('result:', result);
-});
+
+
+// getRepoContributors("jquery", "jquery", function (err, result){
+//   console.log('Errors', err);
+//   console.log('result:', result);
+// });
 
 getRepoContributors('jquery', 'jquery');
