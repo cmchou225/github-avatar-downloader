eafab65ca2bfe5dc14912146b0f821edcb7eab38
Charles <cming.chou@gmail.com>
2017-04-11 00:24:04 +0000

    outlined required main function and added necessary libraries

--- download_avatars.js
+++ download_avatars.js
@@ -1,2 +1,14 @@
 const request = require('request');
+const fs = require('fs');
+
 console.log('Welcome to the GibHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  // request.get()
+
+}
+
+getRepoContributors("jquery", "jquery", function (err, result){
+  console.log('Errors', err);
+  console.log('result:', result);
+});
