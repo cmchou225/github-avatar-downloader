969e4320f93de8e6fe90adee9625f463d6645714
Charles <cming.chou@gmail.com>
2017-04-11 00:41:12 +0000

    added usrname and token and verified correct requestURL will be generated

--- download_avatars.js
+++ download_avatars.js
@@ -3,12 +3,17 @@ const fs = require('fs');
 
 console.log('Welcome to the GibHub Avatar Downloader!');
 
-function getRepoContributors(repoOwner, repoName, cb) {
-  // request.get()
+const GITHUB_USER = "cmchou225",
+      GITHUB_TOKEN = "c2bbe98c0d47f95a8ca29d56e21634093f79f8c4";
 
+function getRepoContributors(repoOwner, repoName, cb) {
+  let requestURL = `https://${GITHUB_USER}:${GITHUB_TOKEN}@api.github.com/repos/${repoOwner}/${repoName}/contributors`;
+  console.log(requestURL);
 }
 
 getRepoContributors("jquery", "jquery", function (err, result){
   console.log('Errors', err);
   console.log('result:', result);
 });
+
+getRepoContributors('jquery', 'jquery');
